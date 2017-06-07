f556c9bb1a6f14684548a7f595f43ca4b6d8b213
singhashish19031998@gmail.com <singhashish19031998@gmail.com>
2017-06-07 17:22:22 +0530

    Controllers are added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def cart_action(current_user_id)
-    if $redis.sismember "cart#{current_user_id}", id
-      "Remove from"
+  protect_from_forgery with: :exception
+  helper_method :current_order
+
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
     else
-      "Add to"
+      Order.new
     end
   end
-
-
-
-  private
-  def current_user
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'carts/show'
-
   devise_for :users , path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :categories
   resources :products
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+
   root 'products#index'
 
 end
